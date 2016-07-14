8f5777eaa14f337247dcae88d0ff0c97048f68d7
Philip Whiting <Himself12794@users.noreply.github.com>
2016-07-13 22:03:59 -0400

    Delete Thing.scala

--- src/main/scala/com/himself12794/main/things/Thing.scala
+++
@@ -1,9 +0,0 @@
-package com.himself12794.main.things
-
-import scala.beans._
-
-class Thing(@BeanProperty var name: String, @BeanProperty var value: String) {
-  override def toString = name + "=" + value;
-}
-
-case class Stuff(name: String, value: String)
